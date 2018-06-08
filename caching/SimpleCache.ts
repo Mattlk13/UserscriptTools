182b4859aaa2435828d1a9a7b069fc25db62e68a
Robert Rudman <rjrudman@gmail.com>
2018-06-08 22:53:27 +1000

    Allow cache clearing to pass an array, so we can more efficiently clear storage

--- caching/SimpleCache.ts
+++ caching/SimpleCache.ts
@@ -19,12 +19,12 @@ export class SimpleCache {
         localStorage.clear();
     }
 
-    public static ClearExpiredKeys(regex?: RegExp) {
+    public static ClearExpiredKeys(regexes?: RegExp[]) {
         const len = localStorage.length;
         for (let i = len - 1; i >= 0; i--) {
             const key = localStorage.key(i);
             if (key) {
-                if (!regex || key.match(regex)) {
+                if (!regexes || regexes.filter(r => key.match(r)).length > 0) {
                     const jsonItem = localStorage.getItem(key);
                     if (jsonItem) {
                         try {
@@ -41,12 +41,12 @@ export class SimpleCache {
         }
     }
 
-    public static ClearAll(regex: RegExp, condition?: (item: string | null) => boolean) {
+    public static ClearAll(regexes: RegExp[], condition?: (item: string | null) => boolean) {
         const len = localStorage.length;
         for (let i = len - 1; i >= 0; i--) {
             const key = localStorage.key(i);
             if (key) {
-                if (key.match(regex)) {
+                if (regexes.filter(r => key.match(r)).length > 0) {
                     if (condition) {
                         const val = localStorage.getItem(key);
                         if (condition(val)) {
