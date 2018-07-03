397473a535aeac5cc1bb7414a6767209db3bcae5
Robert Rudman <rjrudman@gmail.com>
2018-07-03 10:40:18 +1000

    Clear fkey on chat failure and retry

--- chatapi/ChatApi.ts
+++ chatapi/ChatApi.ts
@@ -4,7 +4,14 @@ declare const $: JQueryStatic;
 declare const GM_xmlhttpRequest: any;
 
 export class ChatApi {
+    private static GetExpiryDate(): Date {
+        const expiryDate = new Date();
+        expiryDate.setDate(expiryDate.getDate() + 1);
+        return expiryDate;
+    }
+
     private chatRoomUrl: string;
+
     public constructor(chatUrl: string = 'https://chat.stackoverflow.com') {
         this.chatRoomUrl = `${chatUrl}`;
     }
@@ -23,8 +30,7 @@ export class ChatApi {
             });
         });
 
-        const expiryDate = new Date();
-        expiryDate.setDate(expiryDate.getDate() + 1);
+        const expiryDate = ChatApi.GetExpiryDate();
         return GreaseMonkeyCache.GetAndCache(cachingKey, () => getterPromise, expiryDate);
     }
 
@@ -42,40 +48,54 @@ export class ChatApi {
             });
         });
 
-        const expiryDate = new Date();
-        expiryDate.setDate(expiryDate.getDate() + 1);
+        const expiryDate = ChatApi.GetExpiryDate();
         return GreaseMonkeyCache.GetAndCache(cachingKey, () => getterPromise, expiryDate);
     }
 
     public SendMessage(roomId: number, message: string, providedFkey?: string): Promise<void> {
-        const fkeyPromise = providedFkey
-            ? Promise.resolve(providedFkey)
-            : this.GetChannelFKey(roomId);
+        return new Promise<void>((resolve, reject) => {
+            const requestFunc = () => {
+                const fkeyPromise = providedFkey
+                    ? Promise.resolve(providedFkey)
+                    : this.GetChannelFKey(roomId);
 
-        return fkeyPromise.then((fKey) => {
-            return new Promise<void>((resolve, reject) => {
-                GM_xmlhttpRequest({
-                    method: 'POST',
-                    url: `${this.chatRoomUrl}/chats/${roomId}/messages/new`,
-                    headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-                    data: 'text=' + encodeURIComponent(message) + '&fkey=' + fKey,
-                    onload: (response: any) => {
-                        if (response.status !== 200) {
-                            reject(response.statusText);
-                        } else {
-                            resolve();
-                        }
-                    },
-                    onerror: (response: any) => {
-                        reject(response);
-                    },
+                return fkeyPromise.then((fKey) => {
+                    GM_xmlhttpRequest({
+                        method: 'POST',
+                        url: `${this.chatRoomUrl}/chats/${roomId}/messages/new`,
+                        headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+                        data: 'text=' + encodeURIComponent(message) + '&fkey=' + fKey,
+                        onload: (response: any) => {
+                            if (response.status !== 200) {
+                                onFailure(response.statusText);
+                            } else {
+                                resolve();
+                            }
+                        },
+                        onerror: (response: any) => {
+                            onFailure(response);
+                        },
+                    });
                 });
-            });
+            };
+
+            let numTries = 0;
+            const onFailure = (errorMessage?: string) => {
+                numTries++;
+                if (numTries < 3) {
+                    const fkeyCacheKey = `StackExchange.ChatApi.FKey_${roomId}`;
+                    GreaseMonkeyCache.Unset(fkeyCacheKey);
+                    requestFunc();
+                } else {
+                    reject(errorMessage);
+                }
+            };
+
+            requestFunc();
         });
     }
 
     private GetChannelPage(roomId: number): Promise<string> {
-        const cachingKey = `StackExchange.ChatApi.ChannelData_${roomId}`;
         const getterPromise = new Promise<string>((resolve, reject) => {
             GM_xmlhttpRequest(
                 {
@@ -92,8 +112,6 @@ export class ChatApi {
                 });
         });
 
-        const expiryDate = new Date();
-        expiryDate.setDate(expiryDate.getDate() + 1);
-        return GreaseMonkeyCache.GetAndCache(cachingKey, () => getterPromise, expiryDate);
+        return getterPromise;
     }
 }
